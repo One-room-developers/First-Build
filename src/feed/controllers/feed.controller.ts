9eb7d47b4f8a2665644cd03e9f0e14e25bc532df
Cho-ga-nom <uipjo123@naver.com>
2022-10-23 14:33:04 +0900

    DB 연결 성공

---
+++ src/feed/controllers/feed.controller.ts
@@ -0,0 +1,14 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { FeedPost } from '../models/post.interface';
+import { FeedService } from '../services/feed.service';
+import { Observable } from 'rxjs';
+
+@Controller('feed')
+export class FeedController {
+  constructor(private feedService: FeedService) {}
+
+  @Post()
+  create(@Body() post: FeedPost): Observable<FeedPost> {
+    return this.feedService.createPost(post);
+  }
+}
